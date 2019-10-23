986ade5b975768925c7f47a0cb37c31575ca43f3
sal'e chocho <malachocho01@gmail.com>
2019-10-24 00:00:51 +0200

    Few changes

--- image_uploads.php
+++ image_uploads.php
@@ -7,15 +7,31 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <!-- Use inline css -->
 		<style>
+			video {
+				width: 100px;
+				height: 100px;
+				color: blue;
+			}
 			button.upload {
-				width: 25%;
+			/*	width: 25%;
 				border: none;
 				background: #000;
 				color: white;
-			}
+			*/}
 
 			input.file {
 				visibility: hidden;
+			}
+
+			canvas {
+				display: none;
+				visibility: hidden;
+			}
+
+			i.camera {
+				width: 128px;
+				height: 128px;
+				background: url('images/camera.png');
 			}
 		</style>
 	    <!-- Or link external file -->
@@ -24,11 +40,17 @@
 	<body>
 		<!-- Content goes here -->
 		<div class="stream container">
-			<video autoplay></video>
+			<video autoplay>
+				Click to open camera <i class="camera"></i>
+			</video>
+			<img src="" />
+			<canvas style="display:none;"></canvas>
 		</div>
-		<div class="upload_container">
-			<button class="upload">
-				<input type="file" accept="image/*" class="file"/>
+		<div class="button_container">
+			<button class="trigger" id="trigger">Take Picture</button>
+		
+			<button class="upload" aria-label-for="file">
+				Upload File<input type="file" accept="image/*" class="file" name="file" id="file" />
 			</button>
 		</div>
 		<script type="text/javascript">
@@ -37,18 +59,17 @@
     navigator.mediaDevices.getUserMedia);
 }
 
-if (hasGetUserMedia()) {
-  // Good to go!
-	alert('The nigga has getUserMedia(). Now work!');
-	const constraints = {
-  video: {width: {min: 1280}, height: {min: 720} }};
+function streamCamera() {
+	if (hasGetUserMedia()) {
+		const video = document.querySelector('video');
 
-const video = document.querySelector('video');
-
-navigator.mediaDevices.getUserMedia(constraints).
-  then((stream) => {video.srcObject = stream});
-} else {
-  alert('getUserMedia() is not supported by your browser');
+		navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+			video.srcObject = stream
+		});
+	} else {
+		//You can do better
+  		alert('getUserMedia() is not supported by your browser');
+	}
 }
 		</script>
 	</body>
