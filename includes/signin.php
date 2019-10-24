0b2b5e63d398ef3a2804c1474d2f3280e1c58445
sal'e chocho <malachocho01@gmail.com>
2019-10-25 01:13:41 +0200

    Settings markup

--- includes/signin.php
+++ includes/signin.php
@@ -31,7 +31,8 @@
 	try {
 		//$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 		////$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$stmt = $conn->prepare("SELECT * FROM users WHERE email=? AND pass=SHA1(?)");
+		$p = $p = password_hash($p, PASSWORD_DEFAULT);
+		$stmt = $conn->prepare("SELECT * FROM users WHERE email=? AND pass=?");
 		$stmt->execute();
 		
 		// set the resulting array to associative
--- settings.php
+++ settings.php
@@ -13,5 +13,33 @@
 	<body>
 		<!-- Content goes here -->
 		<h1>Settings</h1>
+
+		<div class="settings_container">
+			<div class="edit username">
+				<p>Your current username is <span>Thanos$$$</span></p>
+				<input type="button" id="edit_username" class="button" value="Change Username" />
+				<form method="POST" id="username_input" class="input hide">
+					<label>
+						<span>New username</span>
+						<input type="text" name="username" />
+					</label>
+					<input type="submit" name="submit" value="Save" />
+				</form>
+			</div>
+			<div class="edit email">
+				<p>Your current email address is <span>themadtitan@hotmail.com</span></p>
+				<input type="button" class="button" value="Change email" />
+				<form method="POST" id="email_input" class="input hide">
+					<label>
+						<span>New email</span>
+						<input type="text" name="email" />
+					</label>
+					<input type="submit" name="submit" value="Save" />
+				</form>
+			</div>
+			<div class="edit password">
+				<p>I want to change my password <div class="icon lock"></div></p>
+			</div>
+		</div>
 	</body>
 </html>
