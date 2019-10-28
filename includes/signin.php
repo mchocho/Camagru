989c7909e124000fba02ae1b774cb48e5ca63ad6
Malesela CHOCHO <mchocho@c5r6s6.wethinkcode.co.za>
2019-10-28 11:32:37 +0200

    Merging

--- includes/signin.php
+++ includes/signin.php
@@ -1,33 +1,36 @@
 <?php
-//require('scream.php');
+require('scream.php');
 
 //echo "hello siginin.php";
 //print_r($_POST);
 
 
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$errors = array();
 
 	if (empty($_POST['username']))
 		$errors[] = 'Please enter your email address.';
 	else
-		$e = trim($_POST['username']);
+		$u = trim($_POST['username']);
 
-	if (empty($_POST['pass']))
+	if (empty($_POST['password']))
 		$errors[] = 'Please enter your password.';
 	else
-		$p = trim($pass);
+		$p = trim($_POST['password']);
 
 	if (!empty($errors))
+	{
+		print_r($errors);
 		return array(false, $error);
+	}
 
-	echo "It works here";
+	echo "It works here<br />";
 
 	try {
 		require_once('sql_connect.php');
 		$p = $p = password_hash($p, PASSWORD_DEFAULT);
-		$stmt = $conn->prepare("SELECT * FROM users WHERE email=? AND pass=?");
-		$stmt->execute();
+		$stmt = $conn->prepare("SELECT * FROM users WHERE username=? AND pass=?");
+		$stmt->execute($u, $p);
 		
 		// set the resulting array to associative
 		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
@@ -46,8 +49,8 @@
 		//You need to handle the result
 		//return array(true, $row);
 	}*/
-	$errors[] = "Your email or password was incorrect.";
-	return array(false, errors);
-//}
+	//$errors[] = "Your email or password was incorrect.";
+	//return array(false, errors);
+}
 
 ?>
