fb8800ba8929170c1f6b786afa40688b792fb365
Malesela CHOCHO <mchocho@c5r6s6.wethinkcode.co.za>
2019-10-31 18:24:12 +0200

    Boooo

--- image_uploads.php
+++ image_uploads.php
@@ -15,10 +15,9 @@
 			video {
 				width: 100%;
 				height: 100%;
-				/*width: 100px;
-				height: 100px;
-				background: blue;*/
+				display: inline-block;
 			}
+
 			button.upload {
 			/*	width: 25%;
 				border: none;
@@ -27,7 +26,6 @@
 			*/}
 
 			input {
-				//visibility: hidden;
 				display: block;
 			}
 
@@ -40,12 +38,14 @@
 				width: 600px;
 				height: 600px;
 				border: 1px solid #000;
+				display: inline-block;
+				margin-left: 6%;
 			}
 
 			.camera {
 				width: 128px;
 				height: 128px;
-				background: url('images/camera.png');
+				background: url('images/icons/camera.png');
 				margin: auto;
 			}
 
@@ -53,27 +53,104 @@
 				display: none;
 				visibility: hidden;
 			}
+
+			.image_gallery {
+				width: 80%;
+				overflow-y: scroll;
+			}
+
+			.image_gallery .img {
+				display: inline-block;
+				width: 30%;
+			}
+
+			.image_gallery button {
+				background: unset;
+				width: 10%;
+				padding: unset;
+				float: right;
+			}
+
+			.button_container {
+				width: 30%;
+				display: inline-block;
+				margin-left: 5%;
+				/*position: relative;*/
+				/*top: 30px;*/
+			}
+
+			.button_container .upload {
+				position: relative;
+				top: 9px;
+			}
+
+			.img .delete {
+				/*position: absolute;*/
+				/*float: left;*/
+			}
+
+			/*.img .delete img {
+				width: 25%;
+				height: 25%;
+				text-align: left;
+			}*/
+
 		</style>
 	    <!-- Or link external file -->
-        <!-- <link rel="stylesheet" href="css/style.css" media="all" /> -->
+        <link rel="stylesheet" href="css/style.css" media="all" />
 	</head>
 	<body>
 		<!-- Content goes here -->
-		<div class="stream container">
-			<span id="hint">Click to open camera <div class="camera"></div></span>
-			<video id="video" autoplay></video>
-			<img id="image" src="" />
-			<canvas></canvas>
+		<header class="header">
+			<a href="index.php">
+				<div class="logo">
+					<img src="images/icons/logo_true.jpg" />
+				</div>
+				<div class="heading">
+					<h1>Mojo</h1>
+				</div>
+			</a>
+			<div class="user_profile_container settings" align="right">
+				<img src="images/mojo.jpg" class="profile_pic" />
+				<span class="username">Thano$$</span>
+				<a href="includes/logout.php" class="logout">Log out</a>
+			</div>
+		</header>
+		<div class="wrapper main settings">
+			<div id="stream_container" class="stream container">
+				<span id="hint">Click to open camera <div class="camera"></div></span>
+				<video id="video" class="hide" autoplay></video>
+				<canvas></canvas>
+			</div>
+			<div class="button_container">
+				<button class="trigger" id="trigger">Take Picture</button>
+				<button class="upload" id="upload" aria-label-for="file">
+					<label>
+						Upload File<input type="file" accept="image/*" class="file" name="file" id="file" />
+					</label>
+				</button>
+			</div>
+			<div class="clip-art_container">
+				<span>A bunch of selectable images go here!</span>
+			</div>
+			<div id="gallery" class="image_gallery">
+				<div class="img">
+					<img src="images/uploads/ocean.jpg" class="item" />
+				</div>
+				<div class="img">
+					<img src="images/uploads/ocean.jpg" class="item" />
+				</div>
+				<div class="img">
+					<img src="images/uploads/ocean.jpg" class="item" />
+				</div>
+				<div class="img">
+					<img src="images/uploads/ocean.jpg" class="item" />
+				</div>
+			</div>
 		</div>
-		<div class="button_container">
-			<button class="trigger" id="trigger">Take Picture</button>
-		
-			<button class="upload" id="upload" aria-label-for="file">
-				<label>
-					Upload File<input type="file" accept="image/*" class="file" name="file" id="file" />
-				</label>
-			</button>
-		</div>
+		<footer>
+			<div>Icons made by <a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+		</footer>
 <script type="text/javascript">
 	const video = document.getElementById('video'),
 	img = document.getElementById('image'),
@@ -99,14 +176,44 @@
 	}
 }*/
 
-	video.addEventListener('click', function() {
+	function FileUpload(img, file) {
+	  const reader = new FileReader();
+	  //this.ctrl = createThrobber(img);
+	  const xhr = new XMLHttpRequest();
+	  this.xhr = xhr;
+
+	  const self = this;
+	  this.xhr.upload.addEventListener("progress", function(e) {
+	        if (e.lengthComputable) {
+	          const percentage = Math.round((e.loaded * 100) / e.total);
+	          //self.ctrl.update(percentage);
+	        }
+	      }, false);
+
+	  xhr.upload.addEventListener("load", function(e){
+	          //self.ctrl.update(100);
+	          //const canvas = self.ctrl.ctx.canvas;
+	          //canvas.parentNode.removeChild(canvas);
+	      }, false);
+	  xhr.open("POST", "http://includes/upload_file.php");
+	  xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
+	  reader.onload = function(evt) {
+	    xhr.send(evt.target.result);
+	  };
+	  reader.readAsBinaryString(file);
+	}
+
+	document.getElementById('stream_container').addEventListener('click', function activateStream() {
 		if (hasGetUserMedia()) {
 			document.getElementById('hint').className += ' hide';
 
-			
+
 			navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+				video.classList.remove('hide');
+
 				video.srcObject = stream;
 				streamActive = true;
+
 			});
 		} else {
 			//You can do better
@@ -116,11 +223,17 @@
 
 	trigger.addEventListener('click', function() {
 		if (streamActive === true) {
+			const img = document.createElement('img'),
+				  gallery = document.getElementById('gallery');
+
 			canvas.width = video.videoWidth;
 			canvas.height = video.videoHeight;
 			canvas.getContext('2d').drawImage(video, 0, 0);
   			// Other browsers will fall back to image/png
-			img.src = canvas.toDataURL('image/webp');
+			img.setAttribute('src', canvas.toDataURL('image/webp'));
+			if (gallery.hasChildNodes())
+				gallery.insertBefore(img, gallery.childNodes[0]);
+			else gallery.appendChild(img);
 		}
 	});
 		</script>
