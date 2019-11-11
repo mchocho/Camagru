3342713f0688a23ed4cc828482fd439d84b1c648
Malesela CHOCHO <mchocho@c5r6s6.wethinkcode.co.za>
2019-11-11 15:34:47 +0200

    Damn

--- config/database.php
+++ config/database.php
@@ -6,6 +6,7 @@ $password   = "654321";
 try {
 	$dbc = new PDO("mysql:host=$servername;dbname=camagru", $username, $password);
 	$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	include('../config/setup.php');
 } catch (PDOException $e) {
 	echo "Connection failed: ".$e->getMessage();
 }
