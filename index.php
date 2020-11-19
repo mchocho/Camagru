f0f3606a1449fa5da16318de1f9ca0f51b930a0a
sal'e chocho <mchocho@student.wethinkcode.co.za>
2020-11-19 12:48:44 +0000

    Move css content in index.php to the styles.css file

--- index.php
+++ index.php
@@ -11,69 +11,9 @@ stfu();
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <title>Camagru</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <!-- Use inline css -->
-	    <style>
-
-.row {
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 4px;
-  margin: auto;
-}
-
-img {
-  width: 400px;
-  height: 400px;
-}
-
-/* Create four equal columns that sits next to each other */
-.column {
-  flex: 25%;
-  max-width: 25%;
-  padding: 0 4px;
-  margin: auto;
-}
-
-.column img {
-  margin-top: 8px;
-  vertical-align: middle;
-  width: 100%;
-}
-
-.wrapper a {
-  position: unset !important;
-  display: flex-inline;
-}
-
-.wrapper a img {
-  /*margin: auto;*/
-} 
-
-.wrapper.main {
-
-}
-
-/* Responsive layout - makes a two column-layout instead of four columns */
-@media screen and (max-width: 800px) {
-  .column {
-    flex: 50%;
-    max-width: 50%;
-  }
-}
-
-/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 600px) {
-  .column {
-    flex: 100%;
-    max-width: 100%;
-  }
-}
-</style>
-	    <!-- Or link external file -->
-        <link rel="stylesheet" href="css/style.css" media="all" />
+      <link rel="stylesheet" href="css/style.css" media="all" />
 	</head>
-	<body>
+	<body class="index">
 		<!-- Content goes here -->
     <header class="header">
       <a href="index.php">
